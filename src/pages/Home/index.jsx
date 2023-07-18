5d841cf46b3a44dfc6f94108a0a5b9a3775b1dab
Arnaldo <costanetoow@gmail.com>
2023-07-17 22:45:02 -0300

    ✨ feat: add banner

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,19 @@
 import Header from '../../components/Header';
 import Menu from '../../components/Menu';
+import styles from './Home.module.scss';
 
 const Home = () => {
 	return (
 		<>
 			<Header />
-			<Menu />
+			<main>
+				<section className={styles.main_section}>
+					<Menu />
+					<div className={styles.main_section__image}>
+						<h1>My travel gallery</h1>
+					</div>
+				</section>
+			</main>
 		</>
 	);
 };
